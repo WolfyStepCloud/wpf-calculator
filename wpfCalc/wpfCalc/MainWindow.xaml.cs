a0351c5d24208e8a7f5465d95f350f068ede7361
wolfy <ilnur.gaet@gmail.com>
2020-05-04 13:37:58 +0600

    fixed bugs, switched to ints, cleaned code

--- wpfCalc/wpfCalc/MainWindow.xaml.cs
+++ wpfCalc/wpfCalc/MainWindow.xaml.cs
@@ -18,136 +18,186 @@ namespace wpfCalc {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        double number1 = 0;
-        double number2 = 0;
-        string operation = "";
+        long number1;
+        long number2;
+        string operation;
+        bool isError;
         public MainWindow() {
             InitializeComponent();
+            Reset();
         }
 
+        #region numbers
         private void btnNumber0_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(0).ToString();
+            if(!isError) 
+                txtDisplay.Text = ReturnNumber(0).ToString();
         }
 
         private void btnNumber1_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(1).ToString();
+            if (!isError)
+                txtDisplay.Text = ReturnNumber(1).ToString();
         }
 
         private void btnNumber2_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(2).ToString();
+            if (!isError)
+                txtDisplay.Text = ReturnNumber(2).ToString();
         }
 
         private void btnNumber3_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(3).ToString();
+            if (!isError)
+                txtDisplay.Text = ReturnNumber(3).ToString();
         }
 
         private void btnNumber4_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(4).ToString();
+            if (!isError)
+                txtDisplay.Text = ReturnNumber(4).ToString();
         }
 
         private void btnNumber5_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(5).ToString();
+            if (!isError)
+                txtDisplay.Text = ReturnNumber(5).ToString();
         }
 
         private void btnNumber6_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(6).ToString();
+            if (!isError)
+                txtDisplay.Text = ReturnNumber(6).ToString();
         }
 
         private void btnNumber7_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(7).ToString();
+            if (!isError)
+                txtDisplay.Text = ReturnNumber(7).ToString();
         }
 
         private void btnNumber8_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(8).ToString();
+            if (!isError)
+                txtDisplay.Text = ReturnNumber(8).ToString();
         }
 
         private void btnNumber9_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = ReturnNumber(9).ToString();
+            if (!isError)
+                txtDisplay.Text = ReturnNumber(9).ToString();
         }
 
+        #endregion
+
+        #region math operations
+
         private void btnPlus_Click(object sender, RoutedEventArgs e) {
-            operation = "+";
-            txtDisplay.Text = "0";
+            if (!isError) {
+                txtDisplay.Text = "+";
+                operation = "+";
+            }
         }
 
         private void btnMinus_Click(object sender, RoutedEventArgs e) {
-            operation = "-";
-            txtDisplay.Text = "0";
+            if (!isError) {
+                operation = "-";
+                txtDisplay.Text = "-";
+            }
         }
 
         private void btnMultiply_Click(object sender, RoutedEventArgs e) {
-            operation = "*";
-            txtDisplay.Text = "0";
+            if (!isError) {
+                operation = "*";
+                txtDisplay.Text = "*";
+            }
         }
 
         private void btnDivide_Click(object sender, RoutedEventArgs e) {
-            operation = "/";
-            txtDisplay.Text = "0";
+            if (!isError) {
+                operation = "/";
+                txtDisplay.Text = "/";
+            }
         }
 
         private void btnEqual_Click(object sender, RoutedEventArgs e) {
-            switch (operation) {
-                case "+":
-                    txtDisplay.Text = (number1 + number2).ToString();
-                    break;
-                case "-":
-                    txtDisplay.Text = (number1 - number2).ToString();
-                    break;
-                case "*":
-                    txtDisplay.Text = (number1 * number2).ToString();
-                    break;
-                case "/":
-                    if(number2 != 0)
-                        txtDisplay.Text = (number1 / number2).ToString();
-                    else
-                        txtDisplay.Text = "Error";
-                    break;
-
+            if (!isError) {
+                switch (operation) {
+                    case "+":
+                        txtDisplay.Text = (number1 + number2).ToString();
+                        break;
+                    case "-":
+                        txtDisplay.Text = (number1 - number2).ToString();
+                        break;
+                    case "*":
+                        txtDisplay.Text = (number1 * number2).ToString();
+                        break;
+                    case "/":
+                        if (number2 != 0)
+                            txtDisplay.Text = (number1 / number2).ToString();
+                        else {
+                            txtDisplay.Text = "Error";
+                            isError = true;
+                        }
+                        break;
+                }
+            }
+        }
+        private void btnPlusMinus_Click(object sender, RoutedEventArgs e) {
+            if (!isError) {
+                if (operation == "") {
+                    number1 *= -1;
+                    txtDisplay.Text = number1.ToString();
+                } else {
+                    number2 *= -1;
+                    txtDisplay.Text = number2.ToString();
+                }
             }
         }
 
+        #endregion
+
+        #region clear operaions
+
         private void btnClearEntry_Click(object sender, RoutedEventArgs e) {
-            if (operation == "")
-                number1 = 0;
-            else
-                number2 = 0;
-            txtDisplay.Text = "0";
+            if (!isError) {
+                if (operation == "")
+                    number1 = 0;
+                else
+                    number2 = 0;
+                txtDisplay.Text = "0";
+            }
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e) {
-            number1 = number2 = 0;
-            operation = "";
-            txtDisplay.Text = "0";
+            Reset();
         }
 
         private void btnBackSpace_Click(object sender, RoutedEventArgs e) {
-            if (operation == "") {
-                number1 = (number1 / 10);
-                txtDisplay.Text = number1.ToString();
-            } else {
-                number2 = (number1 / 10);
-                txtDisplay.Text = number2.ToString();
+            if (!isError) {
+                if (operation == "") {
+                    number1 = (number1 / 10);
+                    txtDisplay.Text = number1.ToString();
+                } else {
+                    number2 = (number1 / 10);
+                    txtDisplay.Text = number2.ToString();
+                }
             }
         }
 
-        private void btnPlusMinus_Click(object sender, RoutedEventArgs e) {
-            if (operation == "") {
-                number1 *= -1;
-                txtDisplay.Text = number1.ToString();
-            } else {
-                number2 *= -1;
-                txtDisplay.Text = number2.ToString();
-            }
-        }
+        #endregion
+
+        #region utility functions
+
         double ReturnNumber(int btnNumber) {
-            if (btnNumber >= 0) {
+            if (btnNumber >= 0 && btnNumber <= 9) {
                 if (operation == "") {
-                    return (number1 * 10) + btnNumber;
+                    return number1 = (number1 * 10) + btnNumber;
                 } else {
-                    return (number2 * 10) + btnNumber;
+                    return number2 = (number2 * 10) + btnNumber;
                 }
             }
             return 0;
         }
+        void Reset() {
+            number1 = 0;
+            number2 = 0;
+            operation = "";
+            isError = false;
+            txtDisplay.Text = "0";
+
+        }
+
+        #endregion
     }
 }
