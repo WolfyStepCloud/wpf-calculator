ee60df5b14767dbdcac5cbe626c75c436a7889cd
wolfy <ilnur.gaet@gmail.com>
2020-05-04 12:25:21 +0600

    func for each number

--- wpfCalc/wpfCalc/MainWindow.xaml.cs
+++ wpfCalc/wpfCalc/MainWindow.xaml.cs
@@ -18,8 +18,111 @@ namespace wpfCalc {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        double number1 = 0;
+        double number2 = 0;
+        string operation = "";
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void btnNumber0_Click(object sender, RoutedEventArgs e) {
+            if(operation == "") {
+                number1 = (number1 * 10);
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10);
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void btnNumber1_Click(object sender, RoutedEventArgs e) {
+            if (operation == "") {
+                number1 = (number1 * 10) + 1;
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10) + 1;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void btnNumber2_Click(object sender, RoutedEventArgs e) {
+            if (operation == "") {
+                number1 = (number1 * 10) + 2;
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10) + 2;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void btnNumber3_Click(object sender, RoutedEventArgs e) {
+            if (operation == "") {
+                number1 = (number1 * 10) + 3;
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10) + 3;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void btnNumber4_Click(object sender, RoutedEventArgs e) {
+            if (operation == "") {
+                number1 = (number1 * 10) + 4;
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10) + 4;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void btnNumber5_Click(object sender, RoutedEventArgs e) {
+            if (operation == "") {
+                number1 = (number1 * 10) + 5;
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10) + 5;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void btnNumber6_Click(object sender, RoutedEventArgs e) {
+            if (operation == "") {
+                number1 = (number1 * 10) + 6;
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10) + 6;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void btnNumber7_Click(object sender, RoutedEventArgs e) {
+            if (operation == "") {
+                number1 = (number1 * 10) + 7;
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10) + 7;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void btnNumber8_Click(object sender, RoutedEventArgs e) {
+            if (operation == "") {
+                number1 = (number1 * 10) + 8;
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10) + 8;
+                txtDisplay.Text = number2.ToString();
+            }
+        }
+
+        private void btnNumber9_Click(object sender, RoutedEventArgs e) {
+            if (operation == "") {
+                number1 = (number1 * 10) + 9;
+                txtDisplay.Text = number1.ToString();
+            } else {
+                number2 = (number1 * 10) + 9;
+                txtDisplay.Text = number2.ToString();
+            }
         }
     }
 }
