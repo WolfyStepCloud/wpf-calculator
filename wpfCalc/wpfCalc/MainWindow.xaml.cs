072b2dd3385eaeee3bd856f612e8c9a7169fe6a3
wolfy <ilnur.gaet@gmail.com>
2020-05-07 17:25:50 +0600

    added funcs for each button except equal

--- wpfCalc/wpfCalc/MainWindow.xaml.cs
+++ wpfCalc/wpfCalc/MainWindow.xaml.cs
@@ -1,14 +1,17 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace wpfCalc {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Calculator
     /// </summary>
     public partial class MainWindow : Window {
         double register;
         string operation;
-        bool valueChanged;
+        bool insertionStarted;  //is not appending
         bool isError;
+        bool complexUsed;
+
         /*
          * TODO:
          * make register 
@@ -46,52 +49,60 @@ namespace wpfCalc {
 
         #region numbers
         private void btnNumber0_Click(object sender, RoutedEventArgs e) {
-            if (valueChanged)
-                txtDisplay.Text += "0";
-            else if (!txtDisplay.Text.Equals("0"))
-                txtDisplay.Text = "0";
+            if (!isError) {
+                if (!insertionStarted) {
+                    txtDisplay.Text = "0";
+                    insertionStarted = true;
+                } else if (!txtDisplay.Text.Equals("0")) {
+                    txtDisplay.Text += "0";
+                }
+            }
+
         }
 
         private void btnNumber1_Click(object sender, RoutedEventArgs e) {
-            addNumberToDisplay(1);
+            if (!isError) AddNumberToDisplay(1);
         }
 
         private void btnNumber2_Click(object sender, RoutedEventArgs e) {
-            addNumberToDisplay(2);
+            if (!isError) AddNumberToDisplay(2);
         }
 
         private void btnNumber3_Click(object sender, RoutedEventArgs e) {
-            addNumberToDisplay(3);
+            if (!isError) AddNumberToDisplay(3);
         }
 
         private void btnNumber4_Click(object sender, RoutedEventArgs e) {
-            addNumberToDisplay(4);
+            if (!isError) AddNumberToDisplay(4);
         }
 
         private void btnNumber5_Click(object sender, RoutedEventArgs e) {
-            addNumberToDisplay(5);
+            if (!isError) AddNumberToDisplay(5);
         }
 
         private void btnNumber6_Click(object sender, RoutedEventArgs e) {
-            addNumberToDisplay(6);
+            if (!isError) AddNumberToDisplay(6);
         }
 
         private void btnNumber7_Click(object sender, RoutedEventArgs e) {
-            addNumberToDisplay(7);
+            if (!isError) AddNumberToDisplay(7);
         }
 
         private void btnNumber8_Click(object sender, RoutedEventArgs e) {
-            addNumberToDisplay(8);
+            if (!isError) AddNumberToDisplay(8);
         }
 
         private void btnNumber9_Click(object sender, RoutedEventArgs e) {
-            addNumberToDisplay(9);
+            if (!isError) AddNumberToDisplay(9);
         }
         private void btnDot_Click(object sender, RoutedEventArgs e) {
             if (!txtDisplay.Text.Contains(",")) {
-                txtDisplay.Text += ",";
-            } else if (!valueChanged)
-                txtDisplay.Text = "0,";
+                if (!insertionStarted) {
+                    txtDisplay.Text = "0,";
+                    insertionStarted = true;
+                } else
+                    txtDisplay.Text += ",";
+            }
         }
 
         #endregion
@@ -99,43 +110,56 @@ namespace wpfCalc {
         #region math operations
 
         private void btnPlus_Click(object sender, RoutedEventArgs e) {
-            addArifOperation("+");
+            if(!isError)
+                AddArifOperation("+");
         }
 
         private void btnMinus_Click(object sender, RoutedEventArgs e) {
-            addArifOperation("-");
+            if (!isError) AddArifOperation("-");
         }
 
         private void btnMultiply_Click(object sender, RoutedEventArgs e) {
-            addArifOperation("*");
+            if (!isError) AddArifOperation("*");
         }
 
         private void btnDivide_Click(object sender, RoutedEventArgs e) {
-            addArifOperation("/");
+            if (!isError) AddArifOperation("/");
         }
 
         private void btnReciprocal_Click(object sender, RoutedEventArgs e) {
+            if (!isError) AddComplexOperation("reciproc");
         }
         private void btnSquareRoot_Click(object sender, RoutedEventArgs e) {
+            if (!isError) AddComplexOperation("sqrt");
+        }
+        private void btnPercent_Click(object sender, RoutedEventArgs e) {
+            if (!isError) PercentOperation();
         }
         private void btnEqual_Click(object sender, RoutedEventArgs e) {
-            
+            if (!isError) ;
         }
         private void btnPlusMinus_Click(object sender, RoutedEventArgs e) {
-            if (txtDisplay.Text == "0") return;
+            if (!isError && !complexUsed) {
+                if (txtDisplay.Text == "0") return;
 
-            if (!txtDisplay.Text.StartsWith("-"))
-                txtDisplay.Text = $"-{txtDisplay.Text}";
-            else
-                txtDisplay.Text = txtDisplay.Text.TrimStart('-');
+                if (!txtDisplay.Text.StartsWith("-"))
+                    txtDisplay.Text = $"-{txtDisplay.Text}";
+                else
+                    txtDisplay.Text = txtDisplay.Text.TrimStart('-');
+                insertionStarted = true;
+            }
         }
 
         #endregion
 
-        #region clear operaions
+        #region clear operations
 
         private void btnClearEntry_Click(object sender, RoutedEventArgs e) {
-            txtDisplay.Text = "0";
+            if (!isError) {
+                txtDisplay.Text = "0";
+                insertionStarted = true;
+            } else
+                Reset();
         }
 
         private void btnClear_Click(object sender, RoutedEventArgs e) {
@@ -149,45 +173,149 @@ namespace wpfCalc {
                 else
                     txtDisplay.Text = txtDisplay.Text.Remove(txtDisplay.Text.Length - 1, 1);
             }
-
         }
 
         #endregion
 
         #region utility functions
 
-        void addNumberToDisplay(int number) {
-            if (number < 1 || number > 9) return;
+        void AddNumberToDisplay(int number) {
+            if (number < 1 || number > 9)
+                throw new ArgumentOutOfRangeException($"Number {nameof(number)} is of of range of 1 to 9");
 
-            if(!valueChanged || isError) {
+            if (isError) {
                 txtDisplay.Text = number.ToString();
-                valueChanged = true;
-            }else
+                isError = false;
+                insertionStarted = true;
+            } else if (txtDisplay.Text.Equals("0") || !insertionStarted) {
+                txtDisplay.Text = number.ToString();
+                insertionStarted = true;
+            } else
                 txtDisplay.Text += number.ToString();
         }
-        void addArifOperation(string op) {
-            if (operation.Equals("/") && txtDisplay.Text.Equals(0)) {
-                txtDisplay.Text = "Error";
-                isError = true;
-            } else if (valueChanged) {
-                txtRegister.Text += $" {txtDisplay.Text} {op}";
+        void AddArifOperation(string op) {
+            if (string.IsNullOrWhiteSpace(op))
+                throw new ArgumentNullException($"String {nameof(op)} is empty.");
+
+            if (insertionStarted) {
+                if (operation != "") {
+                    if (!(operation.Equals("/") && txtDisplay.Text.Equals("0"))) {
+                        double displayValue = GetDisplayValue();
+                        txtRegister.Text += $" {Math.Round(displayValue, 4)} {op}";
+
+                        if (Calculate(register, displayValue, operation, out double result)) {
+                            txtDisplay.Text = Math.Round(result, 4).ToString();
+                            register = result;
+                        } else {
+                            MakeError();
+                            return;
+                        }
+                    } else {
+                        MakeError();
+                        return;
+                    }
+                } else {
+                    double displayValue = GetDisplayValue();
+                    txtRegister.Text += $" {Math.Round(displayValue, 4)} {op}";
+                    register = displayValue;
+                }
+                insertionStarted = false;
             } else {
-                txtRegister.Text = txtRegister.Text.Remove(txtRegister.Text.Length - 1, 1) + op;
+                if (txtRegister.Text.Length != 0) {
+                    if (complexUsed)
+                        txtRegister.Text = txtRegister.Text + " " + op;
+                    else
+                        txtRegister.Text = txtRegister.Text.Remove(txtRegister.Text.Length - 1, 1) + op;
+                } else {
+                    double displayValue = GetDisplayValue();
+                    txtRegister.Text += $"{Math.Round(displayValue, 4)} {op}";
+                    if (operation.Equals("/") && txtDisplay.Text.Equals("0")) {
+                        MakeError();
+                        return;
+                    }
+                }
             }
+            txtDisplay.Text = Math.Round(register, 4).ToString();
             operation = op;
-            valueChanged = false;
+            complexUsed = false;
         }
-        double getDisplayValue() {
+        void AddComplexOperation(string op) {
+            if (!complexUsed) {
+                if (string.IsNullOrWhiteSpace(op))
+                    throw new ArgumentNullException($"String {nameof(op)} is empty.");
+
+                double displayValue = GetDisplayValue();
+                switch (op) {
+                    case "sqrt":
+                        if (displayValue < 0)
+                            MakeError();
+                        register = Math.Sqrt(displayValue);
+                        break;
+                    case "reciproc":
+                        if (displayValue == 0)
+                            MakeError();
+                        register = 1 / displayValue;
+                        break;
+                    default: return;
+                }
+                txtRegister.Text += $" {op}({Math.Round(displayValue, 4)})";
+                txtDisplay.Text = Math.Round(register, 4).ToString();
+                insertionStarted = false;
+                complexUsed = true;
+            }
+        }
+        void PercentOperation() {
+            if (!complexUsed) {
+                register = (GetDisplayValue() / 100) * register;
+                txtRegister.Text += $" {Math.Round(register, 4)}";
+                txtDisplay.Text = $"{Math.Round(register, 4)}";
+                insertionStarted = false;
+                complexUsed = true;
+            }
+        }
+        double GetDisplayValue() {
             return double.Parse(txtDisplay.Text);
         }
+
+        void MakeError() {
+            isError = true;
+            txtDisplay.Text = "Error";
+            insertionStarted = false;
+        }
+        bool Calculate(double firstValue, double secondValue, string operation, out double result) {
+            if (string.IsNullOrWhiteSpace(operation))
+                throw new ArgumentNullException($"Operation string {nameof(operation)} is empty.");
+            result = 0;
+            switch (operation) {
+                case "+":
+                    result = firstValue + secondValue;
+                    break;
+                case "-":
+                    result = firstValue - secondValue;
+                    break;
+                case "*":
+                    result = firstValue * secondValue;
+                    break;
+                case "/":
+                    if (secondValue == 0)
+                        return false;
+
+                    result = firstValue / secondValue;
+                    break;
+                default: return false;
+            }
+            return true;
+        }
+
         void Reset() {
             operation = "";
-            valueChanged = false;
+            insertionStarted = false;
             isError = false;
             register = 0;
             txtDisplay.Text = "0";
             txtRegister.Text = "";
             txtHistory.Text = "";
+            complexUsed = false;
         }
 
         #endregion
